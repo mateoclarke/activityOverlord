8972675ddfb4ad5c66adf9c83040c935078f43d8
mateoclarke <code@mateoclarke.com>
2015-01-20 16:09:01 -0600

    Episode 4: Creating a user account and enabling csrf protection.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,9 +7,25 @@
 
 module.exports = {
 
+	// This loads the sign-up page --> new.ejs
 	'new': function (req, res) {
-		res.view()
+		res.view();
+	},
+
+	// Create a User with the params sent from
+	// the sign-up form --> new.ejs
+	create: function (req, res, next)	{
+		User.create( req.params.all(), function userCreated(err, user) {
+
+			// If there's an error
+			if (err) return next(err);
+
+			// After successfully creating the user
+			// redirect to the show action
+			res.json(user);
+
+		});
 	}
-	
+		
 };
 
--- api/models/User.js
+++ api/models/User.js
@@ -7,6 +7,8 @@
 
 module.exports = {
 
+	schema: true,
+
 	attributes: {
 
 		name: {
@@ -26,6 +28,15 @@ module.exports = {
 
 		encryptedPassword: {
 			type: 'string'
+		}
+
+		toJSON: function () {
+			var obj = this.toObject();
+			delete obj.password;
+			delete obj.confirmation;
+			delete obj.encryptedPassword;
+			delete obj._csrf;
+			return obj;
 		}
 
 	}
