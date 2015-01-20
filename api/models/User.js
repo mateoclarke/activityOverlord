997cd50a6dd52a54126557ff86d0fb674728d4ef
mateoclarke <code@mateoclarke.com>
2015-01-19 23:16:11 -0600

    Create a user model and controller, creating a sign-up page, 'first-look' at sails blueprints.

---
+++ api/models/User.js
@@ -0,0 +1,34 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+	attributes: {
+
+		name: {
+			type: 'string',
+			required: true
+		},
+
+		title: {
+			type: 'string'
+		},	
+
+		email: {
+			type: 'email',
+			unique: true,
+			required: true
+		},	
+
+		encryptedPassword: {
+			type: 'string'
+		}
+
+	}
+	
+};
+
